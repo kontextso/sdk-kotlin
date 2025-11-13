ef08c2f4d45c4209da1d152dcc95c744101ae155
JakubMrozek <jakub.mrozek@gmail.com>
2025-11-13 21:38:49 +0100

    fix warnings

--- ads/src/main/kotlin/so/kontext/ads/internal/data/api/AdsApiImpl.kt
+++ ads/src/main/kotlin/so/kontext/ads/internal/data/api/AdsApiImpl.kt
@@ -3,9 +3,9 @@ package so.kontext.ads.internal.data.api
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.timeout
+import io.ktor.client.request.headers
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
-import io.ktor.client.request.headers
 import so.kontext.ads.internal.data.dto.request.ErrorRequest
 import so.kontext.ads.internal.data.dto.request.PreloadRequest
 import so.kontext.ads.internal.data.dto.response.PreloadResponse
